8346398c77f87ca01e34c1d3f6046fb3d8825dca
Nguyễn Minh Hiếu <hieu24313@gmail.com>
2024-12-12 11:40:34 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { UserModule } from './users/user.module';
 import { UserEntity } from './users/user.entity';
 import { AuthModule } from './auth/auth.module';
 import { ChatModule } from './chat/chat.module';
+
 dotenv.config();
 
 @Module({
@@ -12,15 +13,15 @@ dotenv.config();
     UserModule,
     AuthModule,
     TypeOrmModule.forRoot({
-      type: 'mysql',
+      type: 'postgres',
       host: process.env.DB_HOST ?? 'localhost',
-      port: parseInt(process.env.DB_PORT) ?? 3306,
-      username: process.env.DB_USERNAME ?? 'root',
+      port: parseInt(process.env.DB_PORT) ?? 5432,
+      username: process.env.DB_USERNAME ?? 'postgres',
       password: process.env.DB_PASSWORD ?? 'Admin@123',
       database: process.env.DB_DATABASE ?? 'nestdb',
       entities: [UserEntity],
-      synchronize: true,
-      // logging: true,
+      synchronize: true, // Đảm bảo rằng schema được đồng bộ
+      // logging: true, // Nếu cần bật logging
     }),
     ChatModule,
   ],
